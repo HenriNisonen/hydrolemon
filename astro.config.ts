0cbf315f1b6c92017be878dc4a341acf3a574e60
HenriNisonen <henri@nisonen.com>
2025-05-01 23:36:35 +0300

    Configure GitHub Pages deployment

--- astro.config.ts
+++ astro.config.ts
@@ -23,6 +23,8 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   output: 'static',
+  base: '/',
+  site: 'https://nisonen.com',
 
   integrations: [
     tailwind({
