de60fe7b21e9cd0f5e856e3ebe07691571e30dca
HenriNisonen <henri@nisonen.com>
2025-05-02 00:09:05 +0300

    Update site with correct base URL configuration

--- astro.config.ts
+++ astro.config.ts
@@ -23,8 +23,8 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   output: 'static',
-  base: '/',
-  site: 'https://nisonen.com',
+  base: '/hydrolemon/',
+  site: 'https://nisonen.github.io/hydrolemon',
 
   integrations: [
     tailwind({
